d4d5369416bcff73efc6aca36107edbd10ff775a
Max <maksim0shilov@gmail.com>
2023-03-20 20:50:21 +0200

    [master]: Add classroom progress

--- src/test/java/StudentAppTest.java
+++ src/test/java/StudentAppTest.java
@@ -1,18 +1,36 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeDriver;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class StudentAppTest {
-    @Test
-    public void openStudentApp() {
+    WebDriver driver;
+    private final String APP_URL = "http://acodemystudentapp-env.eba-d2vctp4d.eu-north-1.elasticbeanstalk.com/";
+    @BeforeMethod
+    public void initialize() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
-        WebDriver driver = new ChromeDriver(options);
-        driver.get("http://acodemystudentapp-env.eba-d2vctp4d.eu-north-1.elasticbeanstalk.com/");
-        
+        driver = new ChromeDriver(options);
+        driver.get(APP_URL);
+    }
+    @AfterMethod
+    public void tearDown() {
         driver.close();
         driver.quit();
+    }
+    @Test
+    public void openStudentApp() {
+        WebElement addStudentButton = driver.findElement(By.xpath("//div[@class='ant-table-title']//button"));
+        addStudentButton.click();
+        WebElement nameField= driver.findElement(By.id("name"));
+        nameField.sendKeys("Bill Gates");
+        WebElement emailField = driver.findElement(By.id("email"));
+        emailField.sendKeys("billgates@gmail.com");
+        //    driver.findElement(By.xpath("//div[@class='ant-form-item-control-input-content']//button")).click();
     }
 }
